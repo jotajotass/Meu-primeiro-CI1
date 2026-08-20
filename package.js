7c404449eb74637e7d92cbb79f3997730ad5c19a
jaozin <125150016+jotajotass@users.noreply.github.com>
2026-08-20 10:25:47 -0300

    Update package.js

--- package.js
+++ package.js
@@ -4,3 +4,13 @@ on:
     branches: [main]
   pull_request:
     branches: [main]
+jobs: 
+  build-and-test:
+runs-on: ubuntu-latest
+steps:
+-name: baixar código
+uses: action/checkout@v4
+-name: instalar dependências
+run: npm install
+name: Rodar  testes
+run: npm test
